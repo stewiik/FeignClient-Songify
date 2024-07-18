862d5460351e066fa122e9166b7f3fa0348ff6a6
Wiktoria <w.stec19@gmail.com>
2024-07-18 13:08:00 +0200

    first commit

---
+++ src/main/java/com/feignclientsongify/FeignClientSongifyApplication.java
@@ -0,0 +1,13 @@
+package com.feignclientsongify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeignClientSongifyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FeignClientSongifyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/feignclientsongify/FeignClientSongifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.feignclientsongify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeignClientSongifyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
