bd45869ee1a7d12a5249fe8f453a83a46c297fe3
Wiktoria <w.stec19@gmail.com>
2024-07-18 13:33:17 +0200

    refactor

--- src/main/java/com/feignclientsongify/FeignClientSongifyApplication.java
+++ src/main/java/com/feignclientsongify/FeignClientSongifyApplication.java
@@ -1,7 +1,7 @@
 package com.feignclientsongify;
 
 import com.feignclientsongify.songifyserver.dto.request.CreateSongRequestDto;
-import com.feignclientsongify.songifyserver.dto.response.GetSongResponseDto;
+import com.feignclientsongify.songifyserver.dto.response.*;
 import com.feignclientsongify.songifyserver.dto.request.*;
 import com.feignclientsongify.songifyserver.proxy.SampleSongifyProxy;
 import feign.FeignException;
@@ -28,23 +28,26 @@ public class FeignClientSongifyApplication {
     @EventListener(ApplicationStartedEvent.class)
     public void run() {
         try {
-            sampleSongifyClient.addSong(new CreateSongRequestDto("song5", "Shawn Mendes"));
-            log.info(sampleSongifyClient.fetchAllSongs());
+            CreateSongResponseDto newSong = sampleSongifyClient.addSong(new CreateSongRequestDto("song5", "Shawn Mendes"));
+            log.info("Song added: " + newSong);
+
+            GetAllSongsResponseDto songs = sampleSongifyClient.fetchAllSongs();
+            log.info(songs);
 
             GetSongResponseDto songById = sampleSongifyClient.getSongById(50, "aaa");
             log.info(songById);
 
-            sampleSongifyClient.deleteSong("3");
-            log.info(sampleSongifyClient.fetchAllSongs());
+            DeleteSongResponseDto deletedSong = sampleSongifyClient.deleteSong("3");
+            log.info("Deleted song: " + deletedSong);
 
-            sampleSongifyClient.updateSong(1, new UpdateSongRequestDto("lala", "Madonna"));
-            log.info(sampleSongifyClient.fetchAllSongs());
+            UpdateSongResponseDto updatedSong = sampleSongifyClient.updateSong(1, new UpdateSongRequestDto("lala", "Madonna"));
+            log.info("Updated song: " + updatedSong);
 
-            sampleSongifyClient.partiallyUpdatedSong(1, new PartiallyUpdateSongRequestDto("sooong", "Shawn Mendes"));
-            log.info(sampleSongifyClient.fetchAllSongs());
+            PartiallyUpdateResponseDto partiallyUpdatedSong = sampleSongifyClient.partiallyUpdatedSong(1, new PartiallyUpdateSongRequestDto("sooong", "null"));
+            log.info("Partially updated song: " + partiallyUpdatedSong);
 
         } catch (FeignException e) {
-            log.error(e.getMessage() + "\n" + e.status());
+            log.error(e + "\n" + e.status());
         }
     }
 }
