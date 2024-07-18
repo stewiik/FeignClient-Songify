02c587907070ef8ee05f261357b8f5f13d7a7489
Wiktoria <w.stec19@gmail.com>
2024-07-18 13:09:57 +0200

    add configuration and dependencies

---
+++ src/main/java/com/feignclientsongify/config/Config.java
@@ -0,0 +1,16 @@
+package com.feignclientsongify.config;
+
+import feign.codec.Decoder;
+import feign.jackson.JacksonDecoder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Config {
+
+    @Bean
+    public Decoder feignDecoder() {
+        return new JacksonDecoder();
+    }
+
+}
